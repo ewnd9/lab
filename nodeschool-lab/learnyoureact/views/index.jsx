bab418ac3fb0f5f96108741fafe7fa61a28fb145
ewnd9 <ewndnine@gmail.com>
2016-02-19 18:03:14 +0800

    learnyoureact 2/11

--- nodeschool-lab/learnyoureact/views/index.jsx
+++ nodeschool-lab/learnyoureact/views/index.jsx
@@ -3,7 +3,25 @@ import React from 'react';
 export default class TodoBox extends React.Component{
   render() {
     return <div className="todoBox">
-      Hello, world!
+      <h1>Todos</h1>
+      <TodoList />
+      <TodoForm />
     </div>
   }
-};
+}
+
+class TodoList extends React.Component {
+  render() {
+    return <div className="todoList">
+      I am a TodoList.
+    </div>
+  }
+}
+
+class TodoForm extends React.Component {
+  render() {
+    return <div className="todoForm">
+      I am a TodoForm.
+    </div>
+  }
+}
