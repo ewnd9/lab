fe0fc75312be601d17d72fee203c245ff9a0a90c
ewnd9 <ewndnine@gmail.com>
2016-02-19 18:20:01 +0800

    learnyoureact 5/11

--- nodeschool-lab/learnyoureact/views/index.jsx
+++ nodeschool-lab/learnyoureact/views/index.jsx
@@ -28,8 +28,18 @@ class TodoList extends React.Component {
 }
 
 class Todo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {checked: false};
+  }
+  handleChange(e) {
+    this.setState({checked: e.target.checked});
+  }
   render() {
     return <tr>
+      <td style={{border: "1px solid black"}}>
+        <input type="checkbox" checked={this.state.checked} onChange={this.handleChange}/>
+      </td>
       <td style={{border: "1px solid black"}}>{this.props.title}</td>
       <td style={{border: "1px solid black"}}>{this.props.children}</td>
     </tr>
@@ -37,7 +47,7 @@ class Todo extends React.Component {
 }
 
 Todo.propTypes = {
-  title: React.PropTypes.number.isRequired
+  title: React.PropTypes.string.isRequired
 };
 
 class TodoForm extends React.Component {
