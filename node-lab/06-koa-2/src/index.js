1e93537f28cf082af311789cf84ad0719d0cf033
ewnd9 <ewndnine@gmail.com>
2016-02-25 02:44:15 +0800

    node-lab: koa vk auth

--- node-lab/06-koa-2/src/index.js
+++ node-lab/06-koa-2/src/index.js
@@ -1,16 +1,63 @@
+import sourceMaps from 'source-map-support/register';
+import dotenv from 'dotenv';
+dotenv.config();
+
 import koa from 'koa';
 import convert from 'koa-convert';
 import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import session from 'koa-generic-session';
+import passport from 'koa-passport';
+import VKontakteStrategy from 'passport-vkontakte';
 
 const app = new koa();
-const router = new Router({});
+
+app.use(bodyParser());
+
+app.keys = ['secret']
+app.use(convert(session()));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+function mockUser() {
+  return { id: 1, name: 'ewnd9' };
+};
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  done(null, mockUser());
+});
+
+const vkApp = {
+  clientID: process.env.VK_APP_ID, // VK.com docs call it 'API ID'
+  clientSecret: process.env.VK_APP_SECRET,
+  callbackURL:  'http://localhost:3000/auth/vk/callback'
+};
+
+const vkCallback = function(accessToken, refreshToken, profile, done) {
+  console.log(accessToken);
+  return done(null, mockUser());
+};
+
+passport.use(new VKontakteStrategy.Strategy(vkApp, vkCallback));
+
+const router = new Router();
 
 router.get('/', async ctx => {
   ctx.body = { status: 'hello' };
 });
 
+router.get('/auth/vk', passport.authenticate('vkontakte', { scope: ['friends', 'offline', 'audio'] }));
+router.get('/auth/vk/callback', passport.authenticate('vkontakte'), ctx => {
+  ctx.redirect('/');
+});
+
 router.get('/users/1', async ctx => {
-  const user = await Promise.resolve({ id: 1, name: 'ewnd9' });
+  const user = await Promise.resolve(mockUser());
   ctx.body = user;
 });
 
