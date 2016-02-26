61b0523b0ca385d6c87918dabd7c66cd0361878a
ewnd9 <ewndnine@gmail.com>
2016-02-26 19:20:12 +0800

    web-lab: web-workers-demo complete

--- web-lab/web-workers-demo/scripts/image-app.js
+++ web-lab/web-workers-demo/scripts/image-app.js
@@ -6,6 +6,8 @@
   var canvas = document.querySelector('#image');
   var ctx = canvas.getContext('2d');
 
+  var myWorker = new Worker('/scripts/worker.js');
+
   function handleImage(e){
     var reader = new FileReader();
     reader.onload = function(event){
@@ -40,23 +42,16 @@
 
     toggleButtonsAbledness();
 
-    // Hint! This is where you should post messages to the web worker and
-    // receive messages from the web worker.
+    console.time('xe');
+    myWorker.postMessage({ imageData: imageData, type: type });
+    myWorker.onmessage = function(e) {
+      imageData = e.data;
 
-    length = imageData.data.length / 4;
-    for (i = j = 0, ref = length; 0 <= ref ? j <= ref : j >= ref; i = 0 <= ref ? ++j : --j) {
-      r = imageData.data[i * 4 + 0];
-      g = imageData.data[i * 4 + 1];
-      b = imageData.data[i * 4 + 2];
-      a = imageData.data[i * 4 + 3];
-      pixel = manipulate(type, r, g, b, a);
-      imageData.data[i * 4 + 0] = pixel[0];
-      imageData.data[i * 4 + 1] = pixel[1];
-      imageData.data[i * 4 + 2] = pixel[2];
-      imageData.data[i * 4 + 3] = pixel[3];
-    }
-    toggleButtonsAbledness();
-    return ctx.putImageData(imageData, 0, 0);
+      toggleButtonsAbledness();
+      ctx.putImageData(imageData, 0, 0);
+      
+      console.timeEnd('xe');
+    };
   };
 
   function revertImage() {
