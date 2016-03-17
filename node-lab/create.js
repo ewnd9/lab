419b3073fc5a9123a39ecd2e5608c37287daaefe
ewnd9 <ewndnine@gmail.com>
2016-03-17 21:23:44 +0800

    update create script for node-lab

--- node-lab/create.js
+++ node-lab/create.js
@@ -1,3 +1,7 @@
+#!/usr/bin/env node
+
+'use strict';
+
 const fs = require('fs');
 const dest = process.argv[2];
 
@@ -11,13 +15,9 @@ fs.writeFileSync(`${dest}/.gitignore`, 'node_modules', 'utf-8');
 fs.writeFileSync(`${dest}/index.js`, 'console.log(\'hi\')', 'utf-8');
 fs.writeFileSync(`${dest}/package.json`, JSON.stringify({
   "name": dest,
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
   "private": true,
   "scripts": {
     "test": "echo \"Error: no test specified\" && exit 1"
   },
-  "author": "ewnd9 <ewndnine@gmail.com>",
   "license": "MIT"
 }, null, 2), 'utf-8');
