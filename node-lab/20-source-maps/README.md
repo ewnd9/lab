521b40ca6da31d0f89441365aa4a39cffd038259
ewnd9 <ewndnine@gmail.com>
2016-05-15 22:34:34 +0800

    add node-lab/20-source-maps

---
+++ node-lab/20-source-maps/README.md
@@ -0,0 +1,6 @@
+## Usage
+
+```
+$ npm run build
+$ node test
+```
---
+++ node-lab/20-source-maps/dist/code.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var a = function a() {
+  return 1;
+};
+
+function test() {
+  throw new Error('1');
+}
+
+test();
+//# sourceMappingURL=code.js.map
