2f895b1bbce0191c62d6e3bf26bfac67faf82a05
ewnd9 <ewndnine@gmail.com>
2016-05-18 04:40:37 +0800

    delete 01-youtube-fetch

--- node-lab/01-youtube-fetch/index.js
+++
@@ -1,9 +0,0 @@
-var ytdl = require('ytdl-core');
-
-ytdl.getInfo('https://www.youtube.com/watch?v=lrf6xuFq1Ms', {}, (err, info) => {
-  info.formats
-    .filter(_ => !!_.quality_label && _.container === 'mp4')
-    .forEach(format => {
-      console.log(format);
-    });
-})
