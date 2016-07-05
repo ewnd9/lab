bde54b0403630b8ee7c99febbfb4e91001569376
ewnd9 <ewndnine@gmail.com>
2016-07-05 19:08:11 +0800

    perfschool - 1

--- misc/nodeschool-lab/perfschool/perfschool-playground/using_the_pagespeed_api/solution.js
+++ misc/nodeschool-lab/perfschool/perfschool-playground/using_the_pagespeed_api/solution.js
@@ -6,6 +6,10 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 7777;
 
+var pify = require('pify');
+var psi = require('psi');
+var localtunnel = pify(require('localtunnel'));
+
 app.get('/', home);
 app.get('/insights', insights);
 app.listen(port, listening);
@@ -20,6 +24,21 @@ function home (req, res) {
   res.send(index);
 }
 
-function insights (req, res) {
-  res.end('Not very insightful!');
+function insights (req, res, next) {
+  // get the PageSpeed Insights report
+  localtunnel(port)
+    .then(tunnel => {
+      return psi(tunnel.url);
+    })
+    .then(stats => {
+      res.json({
+        resources: {
+          js: stats.pageStats.numberJsResources,
+          css: stats.pageStats.numberCssResources,
+          total: stats.pageStats.numberResources,
+          hosts: stats.pageStats.numberHosts
+        }
+      });
+    })
+    .catch(err => next(err));
 }
