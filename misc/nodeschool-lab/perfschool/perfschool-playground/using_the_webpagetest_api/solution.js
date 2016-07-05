c369dfe555170112ae577d3af7270bb918d1956c
ewnd9 <ewndnine@gmail.com>
2016-07-05 19:48:19 +0800

    perfschool - 2

--- misc/nodeschool-lab/perfschool/perfschool-playground/using_the_webpagetest_api/solution.js
+++ misc/nodeschool-lab/perfschool/perfschool-playground/using_the_webpagetest_api/solution.js
@@ -6,6 +6,13 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 7777;
 
+var pify = require('pify');
+var localtunnel = pify(require('localtunnel'));
+
+var WebPageTest = require('webpagetest');
+var wpt = new WebPageTest('www.webpagetest.org', process.env.API_KEY);
+var runTest = pify(wpt.runTest.bind(wpt));
+
 app.get('/', home);
 app.get('/test', test);
 app.listen(port, listening);
@@ -20,6 +27,26 @@ function home (req, res) {
   res.send(index);
 }
 
-function test (req, res) {
-  res.end('Not very testful!');
+function test (req, res, err) {
+  localtunnel(port)
+    .then(tunnel => {
+      return runTest(tunnel.url, { location: 'Dulles:Chrome', pollResults: 5, timeout: 600 });
+    })
+    .then(stats => {
+      const data = stats.data.runs[1];
+      const result = {
+        timing: {
+          ttfb: data.firstView.TTFB,
+          speedIndex: data.firstView.SpeedIndex,
+          domLoaded: data.firstView.domContentLoadedEventStart
+        }
+      };
+
+      console.log(result);
+      res.json(result);
+    })
+    .catch(err => {
+      console.log(err);
+      next(err)
+    });
 }
