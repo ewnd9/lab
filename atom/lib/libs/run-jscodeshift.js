380c9987aa3681a71c7df2051599086cfbfbe15d
ewnd9 <ewndnine@gmail.com>
2016-10-06 16:53:33 +0800

    atom: jscodeshift: replace spawning with inline invocaction, add hotkeys

--- atom/lib/libs/run-jscodeshift.js
+++ atom/lib/libs/run-jscodeshift.js
@@ -2,48 +2,66 @@
 /* @flow */
 /* global atom */
 
-import { spawn } from 'child_process';
-
 const defaultTransforms = [
   'js-codemod-cpojer/transforms/no-vars.js',
   'js-codemod-cpojer/transforms/template-literals.js',
   'js-codemod-cpojer/transforms/unchain-variables.js'
 ];
 
-const commonjsToImport = '5to6-codemod/transforms/cjs.js';
-const importToCommonjs = 'js-codemod-ewnd9/transforms/import-to-commonjs.js';
-const redux = 'js-codemod-ewnd9/transforms/redux-replace-switch.js';
+const commonjsToImport = ['5to6-codemod/transforms/cjs.js'];
+const importToCommonjs = ['js-codemod-ewnd9/transforms/import-to-commonjs.js'];
+const redux = ['js-codemod-ewnd9/transforms/redux-replace-switch.js'];
+
+let jscodeshift;
+const cache = {};
 
 export default () => {
   atom.commands.add('atom-workspace', {
     'jscodeshift:defaut': () => run(defaultTransforms),
-    'jscodeshift:import': () => run([commonjsToImport]),
-    'jscodeshift:commonjs': () => run([importToCommonjs]),
-    'jscodeshift:redux-replace-switch': () => run([redux])
+    'jscodeshift:import': () => run(commonjsToImport),
+    'jscodeshift:commonjs': () => run(importToCommonjs),
+    'jscodeshift:redux-replace-switch': () => run(redux)
   });
 };
 
 function run(transforms) {
   const pane = atom.workspace.getActivePane();
   const editor = pane.getActiveEditor();
-  const file = editor.buffer.file.path;
 
-  const promises = transforms
-    .map(t => {
-      const args = [
-        '-t',
-        `${process.env.HOME}/dotfiles/scripts/codemod/${t}`,
-        file
-      ];
+  if (!cache[transforms]) {
+    cache[transforms] = transforms.map(t => require(`${process.env.HOME}/dotfiles/scripts/codemod/${t}`));
+  }
 
-      return () => spawnPromise(`jscodeshift`, args, t);
-    });
+  if (!jscodeshift) {
+    jscodeshift = require('jscodeshift');
+  }
 
-  promises
-    .reduce(
-      (total, curr) => total.then(() => curr()),
-      Promise.resolve()
-    );
+  let source = editor.getSelections()[0].getText();
+  let range;
+
+  if (source.length > 0) {
+    range = editor.getSelections()[0].getBufferRange();
+  } else {
+    source = editor.getText();
+  }
+
+  const result = cache[transforms].reduce(
+    (source, transform) => {
+      try {
+        return transform({ source }, { jscodeshift }, {});
+      } catch (e) {
+        console.log(e)
+        return source;
+      }
+    },
+    source
+  );
+
+  if (range) {
+    editor.setTextInBufferRange(range, result);
+  } else {
+    editor.setText(result);
+  }
 }
 
 function spawnPromise(bin, args, label) {
