93b339b91a7ccaae3394a2d69e751a5c38caebf6
ewnd9 <ewndnine@gmail.com>
2016-11-21 02:39:05 +0800

    atom: snippets-list: spec

---
+++ atom/lib/libs/snippets-list/index.js
@@ -0,0 +1,6 @@
+/*
+- atom: print all snippets
+  - `atom.workspace.open('/home/ewnd9/.atom/snippets/js.cson')`
+  - `<atom-panel class='modal'>` in styleguide
+  - `atom.workspace.getActiveTextEditor().foldAllAtIndentLevel(2)`
+*/
