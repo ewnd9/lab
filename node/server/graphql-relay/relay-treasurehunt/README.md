2457e4090f3477c1fd93aedba4257efc4f3a54a9
ewnd9 <ewndnine@gmail.com>
2016-12-15 02:56:59 +0800

    node: server: relay: init

---
+++ node/server/graphql-relay/relay-treasurehunt/README.md
@@ -0,0 +1,8 @@
+## Tutorial
+
+https://facebook.github.io/relay/docs/tutorial.html#content
+https://facebook.github.io/relay/docs/thinking-in-graphql.html#content
+
+## License
+
+Relay Starter Kit is [BSD licensed](./LICENSE). We also provide an additional [patent grant](./PATENTS).
---
+++ node/server/graphql-relay/relay-treasurehunt/data/database.js
@@ -0,0 +1,46 @@
+/**
+ *  Copyright (c) 2015, Facebook, Inc.
+ *  All rights reserved.
+ *
+ *  This source code is licensed under the BSD-style license found in the
+ *  LICENSE file in the root directory of this source tree. An additional grant
+ *  of patent rights can be found in the PATENTS file in the same directory.
+ */
+
+// Model types
+export class Game {}
+export class HidingSpot {}
+
+// Mock data
+const game = new Game();
+game.id = '1';
+
+const hidingSpots = [];
+(function() {
+  let hidingSpot;
+  const indexOfSpotWithTreasure = Math.floor(Math.random() * 9);
+  for (let i = 0; i < 9; i++) {
+    hidingSpot = new HidingSpot();
+    hidingSpot.id = `${i}`;
+    hidingSpot.hasTreasure = (i === indexOfSpotWithTreasure);
+    hidingSpot.hasBeenChecked = false;
+    hidingSpots.push(hidingSpot);
+  }
+})();
+
+let turnsRemaining = 3;
+
+export function checkHidingSpotForTreasure(id) {
+  if (hidingSpots.some(hs => hs.hasTreasure && hs.hasBeenChecked)) {
+    return;
+  }
+  turnsRemaining--;
+  const hidingSpot = getHidingSpot(id);
+  hidingSpot.hasBeenChecked = true;
+}
+export function getHidingSpot(id) {
+  return hidingSpots.find(hs => hs.id === id);
+}
+export function getGame() { return game; }
+export function getHidingSpots() { return hidingSpots; }
+export function getTurnsRemaining() { return turnsRemaining; }
