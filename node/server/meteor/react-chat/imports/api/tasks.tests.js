4999845536b8084eb3d2957865ea56cd3a4c247c
ewnd9 <ewndnine@gmail.com>
2016-12-15 08:03:53 +0800

    node: meteor example: init

---
+++ node/server/meteor/react-chat/imports/api/tasks.tests.js
@@ -0,0 +1,38 @@
+/* eslint-env mocha */
+
+import { Meteor } from 'meteor/meteor';
+import { Random } from 'meteor/random';
+import { assert } from 'meteor/practicalmeteor:chai';
+
+import { Tasks } from './tasks.js';
+
+if (Meteor.isServer) {
+  describe('Tasks', () => {
+    describe('methods', () => {
+
+      const userId = Random.id();
+      let taskId;
+
+      beforeEach(() => {
+        Tasks.remove({});
+        taskId = Tasks.insert({
+          text: 'test task',
+          createdAt: new Date(),
+          owner: userId,
+          username: 'tmeasday',
+        });
+      });
+
+      it('can delete owned task', () => {
+        // Find the internal implementation of the task method so we can
+        // test it in isolation
+        const deleteTask = Meteor.server.method_handlers['tasks.remove'];
+        const invocation = { userId };
+
+        deleteTask.apply(invocation, [taskId]);
+        assert.equal(Tasks.find().count(), 0);
+      });
+
+    });
+  });
+}
